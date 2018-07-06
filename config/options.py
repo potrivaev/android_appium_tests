5977fcdb1881c89790e9ef8645057eebf1c917ba
yevhenii.potryvaiev <yevhenii.potryvaiev@globallogic.com>
2018-07-06 15:02:51 +0300

    Added first test

--- config/options.py
+++ config/options.py
@@ -1,6 +1,6 @@
 desired_capabilities = {
     "platformName": "Android",
-    "platformVersion": "9",
+    "platformVersion": "8.1.0",
     "deviceName": "Android Emulator",
     "app": "/home/yevhenii/PycharmProjects/mobile_test_task/apk/ApiDemos-debug.apk"
 }
--- tests/test_log_text_box.py
+++ tests/test_log_text_box.py
@@ -7,9 +7,25 @@ from config.options import desired_capabilities, options
 @pytest.fixture(scope="module")
 def driver():
     driver = webdriver.Remote(options["appium_url"], desired_capabilities)
-    yield
+
+    yield driver
     driver.quit()
 
 
 def test_text_appear_after_add_button_pressed(driver):
-    pass
+    text_button = driver.find_elements_by_accessibility_id("Text")[0]
+    text_button.click()
+    elements = ["Linkify", "LogTextBox", "Marquee"]
+    menu_objects = []
+    for element in elements:
+        menu_objects.append(driver.find_elements_by_accessibility_id(element)[0])
+    assert len(menu_objects) == 3
+    menu_objects[1].click()
+    add_button = driver.find_elements_by_accessibility_id("Add")[0]
+    text_view = driver.find_element_by_id("io.appium.android.apis:id/text")
+    text_view_texts = []
+    for i in range(3):
+        add_button.click()
+        text_view_texts.append(text_view.text)
+    assert len(text_view_texts) == 3
+    assert text_view_texts[0] == "This is a test"
