e272f707cba9cb692afbba817dc3ce00d1f0462e
yevhenii.potryvaiev <yevhenii.potryvaiev@globallogic.com>
2018-07-06 17:14:02 +0300

    Moved button pres count to const

--- tests/test_log_text_box.py
+++ tests/test_log_text_box.py
@@ -12,19 +12,21 @@ def driver():
 
 
 def test_text_appear_after_add_button_pressed(driver):
+
     text_button = driver.find_elements_by_accessibility_id("Text")[0]
     text_button.click()
     elements = ["Linkify", "LogTextBox", "Marquee"]
     menu_objects = []
+    add_btn_press_count = 3
     for element in elements:
         menu_objects.append(driver.find_elements_by_accessibility_id(element)[0])
-    assert len(menu_objects) == 3
+    assert len(menu_objects) == add_btn_press_count
     menu_objects[1].click()
     add_button = driver.find_elements_by_accessibility_id("Add")[0]
     text_view = driver.find_element_by_id("io.appium.android.apis:id/text")
     text_view_texts = []
-    for i in range(3):
+    for i in range(add_btn_press_count):
         add_button.click()
         text_view_texts.append(text_view.text)
-    assert len(text_view_texts) == 3
+    assert len(text_view_texts) == add_btn_press_count
     assert text_view_texts[0].replace("\n","") == "This is a test"
